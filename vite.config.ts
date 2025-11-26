cc887566d15405304134141287f1ce1c426803ca
Utenhope <soto95134@hotmail.com>
2025-11-25 21:11:18 -0300

    Solucion final: corregir nombre del repo en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // 💡 USAR RUTA RELATIVA: Esto hace que las referencias en el HTML sean relativas (ej. ./assets/archivo.js)
-  // ESTO IGNORA el nombre exacto del repositorio y funciona siempre.
-  base: './', 
+  // ¡DEBE coincidir con el nombre de tu repositorio: japanece-practice!
+  base: '/japanece-practice/', 
   plugins: [react()],
 })
