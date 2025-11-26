047b12dc58328aa0f76ed430df079d65be3c4e38
Utenhope <soto95134@hotmail.com>
2025-11-25 21:00:47 -0300

    prueba 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Utiliza la variable de entorno VITE_BASE_PATH si existe, sino usa la raíz
+const BASE_PATH = process.env.VITE_BASE_PATH || '/'; 
+
 export default defineConfig({
-  // ¡DEBE ser el nombre exacto de tu repositorio, rodeado de barras!
-  base: '/japanese-practice/', 
+  // Asignamos la variable BASE_PATH, que será la ruta del repo si se ejecuta desde GitHub Actions
+  base: BASE_PATH, 
   plugins: [react()],
 })
